9742dfcd2a44c9c4dca8cb2be1fe2fcfce530bbd
lKavehamdl <kavehahmadi9090@gmail.com>
2024-05-14 14:01:21 +0330

    Message ID added

--- Phase0/messages.sql
+++ Phase0/messages.sql
@@ -4,6 +4,7 @@ phoneNumber2 INT,
 groupID INT,
 groupName VARCHAR(255),
 message VARCHAR(2047),
+messageId INT NOT NULL AUTO_INCREMENT,
 FOREIGN KEY (phonenumber2, groupId) REFERENCES chats(phoneNumber2, groupID)
 );
 
