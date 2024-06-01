4722365ab3c9853f777aec3697faf13379ec7a6d
lKavehamdl <kavehahmadi9090@gmail.com>
2024-06-01 17:30:52 +0330

    for read I added your info and user info updates for update

--- Phase1/KaKaChat/hello.py
+++ Phase1/KaKaChat/hello.py
@@ -3,8 +3,8 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session, g, current_app
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
+import MySQLdb.cursors, datetime, time
+ 
 
 app = Flask(__name__)
 
@@ -16,6 +16,7 @@ app.config['MYSQL_DB'] = 'kakachat'
 mysql = MySQL(app)
 msg = ""
 token = ""
+chatid = 0
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -103,10 +104,14 @@ def login():
                         mysql.connection.commit()
                         print("INSERTED")
                     return redirect(url_for('chats'))
+            global msg
+            msg = "invalid input"
+            return render_template("login.html", msg = msg)
         elif request.method == "GET":
-            return render_template("login.html")
+            msg = ""
+            return render_template("login.html", msg = msg)
     except:
-        return "<h1> mission failed </h1>"
+       return"<h1> mission failed</h1>"
     
 @app.route("/chats", methods=['GET', 'POST'])
 def chats():
@@ -116,7 +121,9 @@ def chats():
         res = cursor.fetchall()
         return render_template("chats.html", chats = res)
     elif request.method == 'POST':
+        global chatid
         chatid = request.form.get('chatID')
+        print(chatid)
         return redirect(url_for('page', id = chatid))
     
 @app.route("/createContact", methods = ["GET", "POST"])
@@ -160,17 +167,112 @@ def createGroupChat():
         cursor.execute("INSERT INTO chats(phoneNumber1, groupName) VALUES(%s, %s)", (token, name))
         mysql.connection.commit()
         return redirect(url_for('chats'))
+    
+@app.route("/addMember", methods = ["GET", "POST"])
+def addMember():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM chats WHERE phoneNumber1 = %s", [token])
+        res = cursor.fetchall()
+        return render_template("addMember.html", res = res)
+    if request.method == "POST":
+        groupID = request.form.get("chatID")
+        phoneNumber = request.form.get("user")
+        cursor.execute("SELECT * FROM chats WHERE chatID = %s", groupID)
+        res = cursor.fetchone()
+        gName = res['groupName']
+        print(gName)
+        cursor.execute("INSERT INTO chats(chatID, phoneNumber1, groupName) VALUES(%s, %s, %s)", (groupID, phoneNumber, gName))
+        mysql.connection.commit()
+        print("DONE!")
+        return redirect(url_for('chats'))
 
 @app.route("/page/<id>", methods = ['POST', 'GET'])
 def page(id):
     print(id)
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     if request.method == "GET":
+        print(" ==== " + id + " ++++++")
         cursor.execute("SELECT * FROM messages WHERE chatID = %s", [id])
         res = cursor.fetchall()
-        for x in res:
-            print(x)
+        # for x in res:
+        #     print(x)
         return render_template("page.html", res = res)
+    elif request.method == "POST":
+        message = request.form.get("message")
+        ts = time.time()
+        timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute("INSERT INTO messages(chatID, message, messageTime, sender) VALUES(%s, %s, %s, %s)", (id, message, timeStamp, token))
+        mysql.connection.commit()
+        print("DONE!")
+        return redirect(url_for('page', id = id))
 
+@app.route("/page/", methods= ["GET", "POST"])
+def test():
+    if request.method == "POST":
+        return redirect(url_for('page', id = chatid), code=307)
+    
+@app.route("/changeInfo", methods =["GET", "POST"])
+def changeInfo():
+    if request.method == "GET":
+        return render_template("PersonalInfo.html")
+    
+@app.route("/changeUsername", methods = ["GET", "POST"])
+def changeUsername():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        return render_template("changeUsername.html")
+    if request.method == "POST":
+        newUsername = request.form.get("username")
+        cursor.execute("UPDATE users SET ID = %s WHERE phoneNumber = %s", (newUsername, token))
+        mysql.connection.commit()
+        print("DONE")
+        return redirect(url_for('chats'))
+    
+@app.route("/changePassword", methods = ["GET", "POST"])
+def changePassword():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        return render_template("changePassword.html")
+    if request.method == "POST":
+        newPassword = request.form.get("password")
+        cursor.execute("UPDATE users SET pass = %s WHERE phoneNumber = %s", (newPassword, token))
+        mysql.connection.commit()
+        print("DONE")
+        return redirect(url_for('chats'))
+    
+@app.route("/changeFirstName", methods = ["GET", "POST"])
+def changeFname():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        return render_template("changeFname.html")
+    if request.method == "POST":
+        newFname = request.form.get("Fname")
+        cursor.execute("UPDATE users SET Fname = %s WHERE phoneNumber = %s", (newFname, token))
+        mysql.connection.commit()
+        print("DONE")
+        return redirect(url_for('chats'))
+    
+@app.route("/changeLastName", methods = ["GET", "POST"])
+def changeLname():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        return render_template("changeLname.html")
+    if request.method == "POST":
+        newLname = request.form.get("Lname")
+        cursor.execute("UPDATE users SET Lname = %s WHERE phoneNumber = %s", (newLname, token))
+        mysql.connection.commit()
+        print("DONE")
+        return redirect(url_for('chats'))
+        
+@app.route("/yourInfo", methods = ["GET", "POST"])
+def Info():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    if request.method == "GET":
+        cursor.execute("SELECT * FROM users WHERE phoneNumber = %s", [token])
+        res = cursor.fetchall()
+        print(res)
+        return render_template("showInfo.html", res = res)
+        
 if __name__ == "__main__":
     app.run()
